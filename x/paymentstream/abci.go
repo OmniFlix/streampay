cc2d5257dd158aa8343786a5eb2d37efa2094737
harish <harishmarri551@gmail.com>
2021-12-03 18:54:20 +0530

    Removed logs

--- x/paymentstream/abci.go
+++ x/paymentstream/abci.go
@@ -59,9 +59,12 @@ func EndBlock(ctx sdk.Context, k *keeper.Keeper) []abcitypes.ValidatorUpdate {
 			totalAmount := ps.TotalAmount.Amount.Int64()
 			percentage := float64(nowTime-startTime) / float64(endTime-startTime)
 			unlockedAmount := float64(totalAmount) * percentage
-			log.Info(fmt.Sprintf("total unlocked amount %f", unlockedAmount))
+			log.Info(fmt.Sprintf("Total unlocked amount %f", unlockedAmount))
 			amountTosend := int64(unlockedAmount) - ps.TotalTransferred.Amount.Int64()
 			amount := sdk.NewCoin(ps.TotalAmount.Denom, sdk.NewInt(amountTosend))
+			if amount.IsZero() || amount.IsNil() {
+				continue
+			}
 			log.Info(fmt.Sprintf("Transferring amount %s to %s", amount.String(), recipient.String()))
 			err = k.TransferAmountFromModuleAccount(ctx, recipient, sdk.NewCoins(amount))
 			if err != nil {
@@ -70,7 +73,7 @@ func EndBlock(ctx sdk.Context, k *keeper.Keeper) []abcitypes.ValidatorUpdate {
 			ps.TotalTransferred = ps.TotalTransferred.Add(amount)
 			ps.LastTransferredAt = ctx.BlockTime()
 			if ps.TotalTransferred.IsGTE(ps.TotalAmount) {
-				log.Info("updating status")
+				log.Info(fmt.Sprintf("Updating payment status.. paymentId: %s", ps.Id))
 				ps.Status = types.StatusCompleted
 			}
 			k.SetPaymentStream(ctx, ps)
