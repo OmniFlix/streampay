88f55be07cd3fa4f233c1ef37c9382c98e07ad6c
harish <harishmarri551@gmail.com>
2021-12-06 16:04:09 +0530

    fix: fix export issues

--- x/paymentstream/types/genesis.go
+++ x/paymentstream/types/genesis.go
@@ -3,10 +3,18 @@ package types
 // DefaultIndex is the default capability global index
 const DefaultIndex uint64 = 1
 
+func NewGenesisState(streams []PaymentStream, nextPaymentStreamNum uint64) *GenesisState {
+	return &GenesisState{
+		PaymentStreamsList:     streams,
+		NextPaymentStreamCount: nextPaymentStreamNum,
+	}
+}
+
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		PaymentStreamsList: []PaymentStream{},
+		PaymentStreamsList:     []PaymentStream{},
+		NextPaymentStreamCount: DefaultIndex,
 		// this line is used by starport scaffolding # genesis/types/default
 	}
 }
