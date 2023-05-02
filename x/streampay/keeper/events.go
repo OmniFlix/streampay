3bd8b405365756a6236ddff0066687c46805a282
Harish Marri <harishmarri551@gmail.com>
2023-05-02 23:21:10 +0530

    add streampay events

---
+++ x/streampay/keeper/events.go
@@ -0,0 +1,63 @@
+package keeper
+
+import (
+	"github.com/OmniFlix/streampay/x/streampay/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"time"
+)
+
+func (k Keeper) emitStreamPaymentTransferEvent(ctx sdk.Context, streamId, recipient string, amount sdk.Coin) {
+	ctx.EventManager().EmitEvents(
+		sdk.Events{
+			sdk.NewEvent(
+				types.EventTypeTransferStreamPayment,
+				sdk.NewAttribute(types.EventAttributePaymentId, streamId),
+				sdk.NewAttribute(types.EventAttributeRecipient, recipient),
+				sdk.NewAttribute(types.EventAttributeAmount, amount.String()),
+			),
+		},
+	)
+}
+func (k Keeper) emitStreamPaymentEndEvent(ctx sdk.Context, streamId, sender string) {
+	ctx.EventManager().EmitEvents(
+		sdk.Events{
+			sdk.NewEvent(
+				types.EventTypeEndStreamPayment,
+				sdk.NewAttribute(types.EventAttributePaymentId, streamId),
+				sdk.NewAttribute(types.EventAttributeSender, sender),
+			),
+		},
+	)
+}
+
+func (k Keeper) emitCreateStreamPaymentEvent(ctx sdk.Context,
+	streamId, sender, recipient string,
+	amount sdk.Coin,
+	paymentType string,
+	endTime time.Time,
+) {
+	ctx.EventManager().EmitEvents(
+		sdk.Events{
+			sdk.NewEvent(
+				types.EventTypeCreateStreamPayment,
+				sdk.NewAttribute(types.EventAttributePaymentId, streamId),
+				sdk.NewAttribute(types.EventAttributeSender, sender),
+				sdk.NewAttribute(types.EventAttributeRecipient, recipient),
+				sdk.NewAttribute(types.EventAttributeAmount, amount.String()),
+				sdk.NewAttribute(types.EventAttributePaymentType, paymentType),
+				sdk.NewAttribute(types.EventAttributeEndTime, endTime.String()),
+			),
+		},
+	)
+}
+func (k Keeper) emitStopStreamPaymentEvent(ctx sdk.Context, streamId, sender string) {
+	ctx.EventManager().EmitEvents(
+		sdk.Events{
+			sdk.NewEvent(
+				types.EventTypeStopStreamPayment,
+				sdk.NewAttribute(types.EventAttributePaymentId, streamId),
+				sdk.NewAttribute(types.EventAttributeSender, sender),
+			),
+		},
+	)
+}
---
+++ x/streampay/types/events.go
@@ -0,0 +1,15 @@
+package types
+
+const (
+	EventTypeTransferStreamPayment = "transfer_stream_payment"
+	EventTypeEndStreamPayment      = "end_stream_payment"
+	EventTypeCreateStreamPayment   = "create_stream_payment"
+	EventTypeStopStreamPayment     = "stop_stream_payment"
+
+	EventAttributePaymentId   = "payment-id"
+	EventAttributeSender      = "sender"
+	EventAttributeRecipient   = "recipient"
+	EventAttributeAmount      = "amount"
+	EventAttributePaymentType = "payment-type"
+	EventAttributeEndTime     = "endtime"
+)
