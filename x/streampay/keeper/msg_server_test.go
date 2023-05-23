3cc1438fd34cbf9619c70e86edf88c90ce01520d
Harish Marri <harishmarri551@gmail.com>
2023-05-23 23:01:38 +0530

    tests: added more test cases

--- x/streampay/keeper/msg_server_test.go
+++ x/streampay/keeper/msg_server_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/OmniFlix/streampay/x/streampay/types"
@@ -33,12 +34,89 @@ func (suite *KeeperTestSuite) TestStreamSendMsg() {
 			valid:                 true,
 			expectedMessageEvents: 1,
 		},
+		{
+			sender:                suite.TestAccs[0].String(),
+			recipient:             suite.TestAccs[1].String(),
+			amount:                sdk.NewInt64Coin(types.DefaultParams().StreamPaymentFee.Denom, 10_000_000),
+			streamType:            types.TypeContinuous,
+			duration:              time.Second * 100,
+			periods:               nil,
+			cancellable:           false,
+			fee:                   sdk.NewInt64Coin(types.DefaultParams().StreamPaymentFee.Denom, 1_000_000),
+			valid:                 false,
+			expectedMessageEvents: 0,
+		},
+		{
+			sender:                suite.TestAccs[0].String(),
+			recipient:             suite.TestAccs[1].String(),
+			amount:                sdk.NewInt64Coin(types.DefaultParams().StreamPaymentFee.Denom, 100_000_000),
+			streamType:            types.TypeDelayed,
+			duration:              time.Second * 100,
+			periods:               nil,
+			cancellable:           false,
+			fee:                   sdk.NewInt64Coin(types.DefaultParams().StreamPaymentFee.Denom, 10_000_000),
+			valid:                 true,
+			expectedMessageEvents: 1,
+		},
+		{
+			sender:     suite.TestAccs[0].String(),
+			recipient:  suite.TestAccs[1].String(),
+			amount:     sdk.NewInt64Coin(types.DefaultParams().StreamPaymentFee.Denom, 100_000_000),
+			streamType: types.TypePeriodic,
+			duration:   time.Second * 100,
+			periods: []*types.Period{
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+				{
+					Amount:   1_000_000,
+					Duration: 10,
+				},
+			},
+			cancellable:           true,
+			fee:                   sdk.NewInt64Coin(types.DefaultParams().StreamPaymentFee.Denom, 10_000_000),
+			valid:                 true,
+			expectedMessageEvents: 1,
+		},
 	} {
 		suite.Run("create stream payment", func() {
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
 			// Test stream send message
-			suite.msgServer.StreamSend(
+			_, err := suite.msgServer.StreamSend(
 				sdk.WrapSDKContext(ctx),
 				types.NewMsgStreamSend(
 					tc.sender,
@@ -51,6 +129,10 @@ func (suite *KeeperTestSuite) TestStreamSendMsg() {
 					tc.fee,
 				),
 			)
+			if tc.valid {
+				suite.Require().NoError(err)
+			}
+			fmt.Println(tc)
 			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.EventTypeCreateStreamPayment, tc.expectedMessageEvents)
 		})
@@ -59,6 +141,8 @@ func (suite *KeeperTestSuite) TestStreamSendMsg() {
 
 func (suite *KeeperTestSuite) TestStopStreamMsg() {
 	suite.CreateDefaultStreamPayment(true)
+	sp2 := suite.CreateStreamPayment(types.TypeDelayed, false)
+	sp3 := suite.CreateStreamPayment(types.TypeContinuous, true)
 	for _, tc := range []struct {
 		streamId              string
 		sender                string
@@ -70,6 +154,18 @@ func (suite *KeeperTestSuite) TestStopStreamMsg() {
 			sender:                suite.TestAccs[0].String(),
 			valid:                 true,
 			expectedMessageEvents: 1,
+		},
+		{
+			streamId:              sp2,
+			sender:                suite.TestAccs[0].String(),
+			valid:                 false,
+			expectedMessageEvents: 0,
+		},
+		{
+			streamId:              sp3,
+			sender:                suite.TestAccs[1].String(),
+			valid:                 false,
+			expectedMessageEvents: 0,
 		},
 	} {
 		suite.Run("stop stream", func() {
@@ -83,7 +179,9 @@ func (suite *KeeperTestSuite) TestStopStreamMsg() {
 					tc.sender,
 				),
 			)
-			suite.Require().NoError(err)
+			if tc.valid {
+				suite.Require().NoError(err)
+			}
 			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.EventTypeStopStreamPayment, tc.expectedMessageEvents)
 			suite.AssertEventEmitted(ctx, types.EventTypeEndStreamPayment, tc.expectedMessageEvents)
@@ -106,6 +204,12 @@ func (suite *KeeperTestSuite) TestClaimStreamedAmountMsg() {
 			valid:                 true,
 			expectedMessageEvents: 1,
 		},
+		{
+			streamId:              suite.defaultStreamPaymentId,
+			recipient:             suite.TestAccs[0].String(),
+			valid:                 false,
+			expectedMessageEvents: 0,
+		},
 	} {
 		suite.Run("claim streamed amount", func() {
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
@@ -118,7 +222,9 @@ func (suite *KeeperTestSuite) TestClaimStreamedAmountMsg() {
 					tc.recipient,
 				),
 			)
-			suite.Require().NoError(err)
+			if tc.valid {
+				suite.Require().NoError(err)
+			}
 			// Ensure current number and type of event is emitted
 			suite.AssertEventEmitted(ctx, types.EventTypeClaimStreamedAmount, tc.expectedMessageEvents)
 
