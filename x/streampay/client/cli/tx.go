0ac72888fb00314ef3225fff707ef1a467910198
Harish Marri <harishmarri551@gmail.com>
2023-06-15 01:25:55 +0530

    update cli documentation

--- x/streampay/client/cli/tx.go
+++ x/streampay/client/cli/tx.go
@@ -37,11 +37,38 @@ func GetTxCmd() *cobra.Command {
 // GetCmdStreamSend implements the stream-send command
 func GetCmdStreamSend() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:  "stream-send",
-		Long: "creates a stream payment",
+		Use:   "stream-send",
+		Short: "creates a stream payment",
+		Long: "creates a stream payment with the given recipient and amount.\n" +
+			"stream payment will start immediately aftet the successful execution of this command.\n" +
+			"types of stream-payments:\n" +
+			"1. continuous: stream payment will be sent continuously until the end time is reached\n" +
+			"2. delayed: stream payment will be sent after the end time is reached (use --delayed flag)\n" +
+			"3. periodic: stream payment will be sent periodically until the end time is reached" +
+			" (use --stream-periods-file)\n\n" +
+			"example stream-periods-file:\n" +
+			"[\n" +
+			"	{\n" +
+			"		\"amount\": 100000,\n" +
+			"		\"duration\": 60\n" +
+			"	},\n" +
+			"	{\n" +
+			"		\"amount\": 100000,\n" +
+			"		\"duration\": 60\n" +
+			"	},\n" +
+			"]\n\n" +
+			"cancellation of stream payments:\n" +
+			"using --cancellable flag, you can create a cancellable stream payment\n",
 		Example: fmt.Sprintf(
-			"$ %s tx streampay stream-send [recipient] [amount] --end-time <end-timestamp> ",
-			version.AppName,
+			"$ %s tx streampay stream-send [recipient] [amount] --end-time <end-timestamp>"+
+				" --chain-id <chain-id> --from <sender> --fees <fees>\n\n"+
+				"delayed payment:\n"+
+				"$ %s tx streampay stream-send [recipient] [amount] --end-time <end-timestamp> --delayed"+
+				" --chain-id <chain-id> --from <sender> --fees <fees>\n\n"+
+				"periodic payment:\n"+
+				"$ %s tx streampay stream-send [recipient] [amount] --end-time <end-timestamp> --stream-periods-file <stream-periods-file>"+
+				" --chain-id <chain-id> --from <sender> --fees <fees>\n\n",
+			version.AppName, version.AppName, version.AppName,
 		),
 		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -135,10 +162,14 @@ func GetCmdStreamSend() *cobra.Command {
 // GetCmdStopStream implements the stop-stream command
 func GetCmdStopStream() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:  "stop-stream",
-		Long: "stops a stream payment",
+		Use:   "stop-stream",
+		Short: "stops a stream payment",
+		Long: "stops a stream payment with the given stream id if stream-payment is cancellable.\n" +
+			"if stream-payment is not cancellable, this command will fail.\n" +
+			"if payment is in progress only remaining amount will be refunded to the sender.\n" +
+			"streamed amount can be claimed using claim command.\n",
 		Example: fmt.Sprintf(
-			"$ %s tx streampay stop-stream [stream-id]",
+			"$ %s tx streampay stop-stream [stream-id] --chain-id <chain-id> --from <sender> --fees <fees>",
 			version.AppName,
 		),
 		Args: cobra.ExactArgs(1),
@@ -161,10 +192,11 @@ func GetCmdStopStream() *cobra.Command {
 
 func GetCmdClaimStreamedAmount() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:  "claim",
-		Long: "claim streamed amount",
+		Use:   "claim",
+		Short: "claim streamed amount",
+		Long:  "claim streamed amount for the given stream id.\n",
 		Example: fmt.Sprintf(
-			"$ %s tx streampay claim [stream-id]",
+			"$ %s tx streampay claim [stream-id] --chain-id <chain-id> --from <sender> --fees <fees>",
 			version.AppName,
 		),
 		Args: cobra.ExactArgs(1),
