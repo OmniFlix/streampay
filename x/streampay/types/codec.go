ca6db367f7b60c988fce09fa72454608c46287e0
Marri Harish <harishmarri551@gmail.com>
2023-12-04 15:03:27 +0530

    fix amino signing issues (#23)

--- x/streampay/types/codec.go
+++ x/streampay/types/codec.go
@@ -3,27 +3,29 @@ package types
 import (
 	"github.com/OmniFlix/streampay/v2/x/streampay/exported"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
+	govcodec "github.com/cosmos/cosmos-sdk/x/gov/codec"
 )
 
 const (
 	AminoTypeStreamSendMsg          = "OmniFlix/streampay/MsgStreamSend"
 	AminoTypeStopStreamMsg          = "OmniFlix/streampay/MsgStopStream"
-	AminoTypeClaimStreamedAmountMsg = "OmniFlix/streampay/MsgClaimStreamedAmount"
+	AminoTypeClaimStreamedAmountMsg = "OmniFlix/streampay/MsgClaimStream"
 	AminoTypeStreamPayment          = "OmniFlix/streampay/StreamPayment"
 	AminoTypeUpdateParamsMsg        = "OmniFlix/streampay/MsgUpdateParams"
 	AminoTypeParams                 = "OmniFlix/streampay/Params"
 )
 
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgStreamSend{}, AminoTypeStreamSendMsg, nil)
-	cdc.RegisterConcrete(&MsgStopStream{}, AminoTypeStopStreamMsg, nil)
-	cdc.RegisterConcrete(&MsgClaimStreamedAmount{}, AminoTypeClaimStreamedAmountMsg, nil)
-	cdc.RegisterConcrete(&MsgUpdateParams{}, AminoTypeUpdateParamsMsg, nil)
+	legacy.RegisterAminoMsg(cdc, &MsgStreamSend{}, AminoTypeStreamSendMsg)
+	legacy.RegisterAminoMsg(cdc, &MsgStopStream{}, AminoTypeStopStreamMsg)
+	legacy.RegisterAminoMsg(cdc, &MsgClaimStreamedAmount{}, AminoTypeClaimStreamedAmountMsg)
+	legacy.RegisterAminoMsg(cdc, &MsgUpdateParams{}, AminoTypeUpdateParamsMsg)
 
 	cdc.RegisterInterface((*exported.StreamPaymentI)(nil), nil)
 	cdc.RegisterConcrete(&StreamPayment{}, AminoTypeStreamPayment, nil)
@@ -54,10 +56,10 @@ var (
 func init() {
 	RegisterLegacyAminoCodec(amino)
 	cryptocodec.RegisterCrypto(amino)
-	amino.Seal()
 
 	// Register all Amino interfaces and concrete types on the authz Amino codec
 	// so that this can later be used to properly serialize MsgGrant and MsgExec
 	// instances.
 	RegisterLegacyAminoCodec(authzcodec.Amino)
+	RegisterLegacyAminoCodec(govcodec.Amino)
 }
