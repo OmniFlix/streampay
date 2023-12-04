c29ccbddb21e0f329f1e915825340e7b59da6cb5
Harish Marri <harishmarri551@gmail.com>
2023-12-04 16:26:38 +0530

    update swagger api doc scripts

--- scripts/protoc_swagger_openapi_gen.sh
+++ scripts/protoc_swagger_openapi_gen.sh
@@ -1,41 +1,22 @@
 #!/usr/bin/env bash
 
-# Run from the project root directory
-# This script generates the swagger & openapi.yaml documentation for the rest API on port 1317
-
-# change to the scripts folder
-cd "$(dirname `realpath "$0"`)"
-# change to the root folder
-cd ../
+set -eo pipefail
 
 mkdir -p ./tmp-swagger-gen
-
-# Get the paths used repos from go/pkg/mod
-cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
-
-
-proto_dirs=$(find ./proto "$cosmos_sdk_dir"/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+cd proto
+proto_dirs=$(find ./OmniFlix -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
   if [[ ! -z "$query_file" ]]; then
-    # Get swagger protoc plugin with `go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v1.16.0`
-    protoc  \
-    -I "proto" \
-    -I "$cosmos_sdk_dir/third_party/proto" \
-    -I "$cosmos_sdk_dir/proto" \
-     "$query_file" \
-    --swagger_out ./tmp-swagger-gen \
-    --swagger_opt logtostderr=true \
-    --swagger_opt fqn_for_swagger_name=true \
-    --swagger_opt simple_operation_ids=true
+    buf generate --template buf.gen.swagger.yaml $query_file
   fi
 done
 
-# Fix circular definition in cosmos/tx/v1beta1/service.swagger.json
-jq 'del(.definitions["cosmos.tx.v1beta1.ModeInfo.Multi"].properties.mode_infos.items["$ref"])' ./tmp-swagger-gen/cosmos/tx/v1beta1/service.swagger.json > ./tmp-swagger-gen/cosmos/tx/v1beta1/fixed-service.swagger.json
-rm ./tmp-swagger-gen/cosmos/tx/v1beta1/service.swagger.json
+cd ..
+# combine swagger files
+# uses nodejs package `swagger-combine`.
+
 
 # Tag everything as "gRPC Gateway API"
 perl -i -pe 's/"(Query|Service)"/"gRPC Gateway API"/' $(find ./tmp-swagger-gen -name '*.swagger.json' -print0 | xargs -0)
